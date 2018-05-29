8971ce500b7b2ebf8ca7111a2602b8b4ef9371fd
dudemanguy <random342@airmail.cc>
2018-05-29 12:18:54 -0500

    check for existence before copying database

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,11 @@
 .onLoad <- function(libname, pkgname) {
 	from <- system.file("extdata", "annotations.sqlite", package="geneConvert")
 	directory <- file.path(path.expand("~"), ".config/geneConvert")
-	dir.create(directory)
+	if (!file.exists(directory)) {
+		dir.create(directory)
+	}
 	to <- file.path(directory, "annotations.sqlite")
-	file.copy(from, to, copy.mode=FALSE)
+	if (!file.exists(to)) {
+		file.copy(from, to, copy.mode=FALSE)
+	}
 }
